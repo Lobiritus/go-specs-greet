363970ed995c6206fa1a69f6844c45567bdd85b1
Ali Shykhyev <aa@rossi.ru>
2024-12-02 16:49:23 +0300

    Added part of Acceptance test

---
+++ specifications/greet.go
@@ -0,0 +1,18 @@
+package specifications
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Greeter interface {
+	Greet(name string) (string, error)
+}
+
+func GreetSpecification(t testing.TB, greeter Greeter) {
+	got, err := greeter.Greet("Mike")
+	assert.NoError(t, err)
+	assert.Equal(t, got, "Hello, Mike!")
+
+}
